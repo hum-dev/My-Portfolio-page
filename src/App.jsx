0fb4856a51ec71430d86103072f780370a6364d0
hum-dev <humphreymuriungi@outlook.com>
2023-05-22 12:37:33 +0300

    conflict

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,8 @@ import Intro from "./components/intro/intro";
 import ProductList from "./components/ProductList/ProductList";
 import Contact from "./components/Contact/Contact";
 
-<<<<<<< HEAD
 
-=======
 import Nav from "./components/Navigation/Nav";
->>>>>>> 572156648a9ed284bc44f61ee55429be5e28e2e2
 
 const App = () => {
   return <div>
